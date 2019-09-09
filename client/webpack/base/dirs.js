534530eb7f1675e14d76c92a922d75451eefab84
burchin <burchin@burchindeMacBook-Pro.local>
2019-09-09 16:17:08 +0800

    更改目录

--- client/webpack/base/dirs.js
+++ client/webpack/base/dirs.js
@@ -1,11 +1,11 @@
 const { resolve } = require('path');
-const root = resolve(__dirname, `../../${process.env.npm_package_clientPath}`);
+const root = resolve(__dirname, '../../');
 
 module.exports = {
   // 根目录
   root: root,
   // 配置目录
-  // config: resolve(root, './webpack'),
+  config: resolve(root, './webpack'),
   // 源码目录
   src: resolve(root, './src'),
   // 多页面目录
@@ -13,7 +13,7 @@ module.exports = {
   // vendor
   lib: resolve(root, './lib'),
   // 生成目录
-  dist: resolve(root, './dist')
+  dist: resolve(root, './dist'),
   // modules
-  // modules: resolve(root, './node_modules')
+  modules: resolve(root, './node_modules')
 };
